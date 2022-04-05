6b428ffa05ed427103c31d44672f81ed7e5dd239
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-04-05 15:43:37 -0700

    updated source

---
+++ py_guess.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+i = 0
+p = 1
+
+while True:
+
+    age = input("Input 0 or 1: ")
+
+    if age == 1:
+        print("One")
+
+    if age == 0:
+        print("Zero")
+
+    else:
+        print("Incorrect value")
+
+    print(f"So, you're {i}, {o}..")
+        
